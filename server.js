3e76ed665aa7af3361421536e772e4b7ec02a7ea
Howler234 <howler234@gmail.com>
2023-06-04 16:23:38 -0600

    Removed specifications for assignment

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-/*Enhance your server to be able to handle the following commands from clients. In all cases you should log the result to server.log.
-
-/w - Sends a whisper to another connected client. For example: ‘/w Guest3 Hi’ Should send a message to Guest3 only.
-Your server should send an informative error message if the command fails for any reason (incorrect number of inputs, invalid username, trying to whisper themselves etc.)
-If there is no error then a private message containing the whisper sender’s name as well as the whispered message should be sent to the indicated user
-
-
-/kick - Kicks another connected client, as long as the supplied admin password is correct. (You can just store an adminPassword variable in memory on your server for now.) For example ‘/kick Guest3 supersecretpw’ should kick Guest3 from the chat
-Your server should send an informative error message if the command fails for any reason (incorrect number of inputs, incorrect admin password, trying to kick themselves, invalid username to kick, etc)
-If there is no error then a private message should be sent to the kicked user informing them that they have been kicked from the chat. They should then be removed from the server. A message should be broadcast to all other users informing them that the kicked user left the chat.
-*/
-
-
 const net = require('net');
 const fs = require('fs');
 const file = fs.createWriteStream('./chat.log', {flags: 'a'});
